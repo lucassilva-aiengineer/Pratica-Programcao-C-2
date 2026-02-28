dfb560b11cb07f081e44ad2f43208aecf803468f
lucassilva-aiengineer <lucassilva.aiengineer@gmail.com>
2026-02-28 20:43:41 -0300

    Adicionando novas partes

--- projetoA/projeto1.c
+++ projetoA/projeto1.c
@@ -10,7 +10,7 @@ int main(){
 
     while (opcao != 'Q'){
 
-        printf("Para Adicao Digite A.\n");
+        printf("\nPara Adicao Digite A.\n");
         printf("Indique A Sua Opcao: ");
         scanf("%c", &opcao); 
 
@@ -61,7 +61,55 @@ int main(){
 
 
         }
-            
+
+        // O caso da múltiplicação. 
+        case 'M': {
+
+            double fatorA; 
+            double fatorB; 
+
+            printf("\n\n============= Multiplicacao ============\n\n"); 
+
+            printf("Indique o primeiro fator: ");
+            scanf("%lf", &fatorA); 
+
+            printf("Indique o segundo fator: ");
+            scanf("%lf", &fatorB); 
+
+            double resultado = fatorA * fatorB; 
+
+            printf("\n%.2lf x %.2lf = %.2lf.\n", fatorA, fatorB, resultado);
+
+        }
+
+        case 'D': {
+
+            double numerador; 
+            double denominador; 
+
+            printf("\n\n============ Divisao =============\n\n");
+
+            printf("Indique o numerador: ");
+            scanf("%lf", numerador); 
+
+            printf("Indique o denominador: ");
+            scanf("%lf", denominador); 
+
+            if (denominador != 0){
+
+                double resultado = numerador / denominador; 
+
+                printf("\n\n============== Resultado =============\n\n"); 
+
+                printf("\n\n%.2lf : %.2lf = %.2lf.\n\n", numerador, denominador, resultado);
+
+            } else {
+
+                printf("O denominador nao pode ser igual a zero; Isso nos levaria a uma indeterminacao em matamática.");
+            }
+        } 
+
+        // O código está com um problema relacionado ao que está sendo exibido no display, uma repetição na frase: "Para Adição que aparece duas ". Problema resolvido.
     }
 
     }
